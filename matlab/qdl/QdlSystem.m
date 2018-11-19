48b42cb5a6f4651360dc74085fa8c5847849e6a0
Joseph Hood <josephmhood@gmail.com>
2018-11-19 09:23:17 -0500

    added docs

--- matlab/qdl/QdlSystem.m
+++ matlab/qdl/QdlSystem.m
@@ -1,4 +1,33 @@
 
+%
+%  n   := number of nodes
+%  m   := number of branches
+%
+%  a(n, m) := incidence matrix between node i and branch k
+%             0: no connection
+%             1: ith node of branch k connected to node i
+%            -1: jth node of branch k connected to node i
+%
+%  b(n, n) := vccs gain matrix between node i and node p
+%  s(n, m) := cccs gain matrix between node i and branch k
+%  t(m, n) := vcvs gain matrix between branch k and node i
+%  z(m, m) := ccvs gain matrix between branch k and branch q 
+%
+%  c(n) := capacitance at node i
+%  g(n) := conductance at node i
+%  h(n) := current injection at node i
+%
+%  l(n) := series inductance in branch k
+%  r(n) := series resistance in branch k
+%  e(n) := voltage at branch k from node i to node j
+%
+%  where:
+% 
+%  vi = 1/ci * (hi + bip * vp + siq * iq + sum(ik) - gi * vi)
+%
+%  ik = 1/lk * (ek + tkp * vp + zkq * iq + (vik - vjk) - rk * ik)
+%
+%
 
 
 classdef QdlSystem < handle
@@ -15,7 +44,6 @@ classdef QdlSystem < handle
         q;    % quantum states
         d;    % internal state derivative
         dQ;   % quantum levels
-        
 
     end
     
